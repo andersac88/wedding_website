0ecda8ba074823a2f351bed62e561a36050a24f9
Andrew Andersen <andersac88@gmail.com>
2019-03-20 00:05:57 -0500

    updates

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,7 @@ let partyArray = [
       name: "Jeff Krantz",
       Role: "Best Man",
       Photo: './Images/jeff.png',
-      Detail: "Something Something Something"
+      Detail: "Jeff and Andrew met at the bus stop on the first day of kindergarden. With no regard for their sons' opinions, their mother's insisted that they become friends. Fortunately, like for a majority of arranged marriages, the parents were right."
     },
     {
     name: "Sara St. Aubin",
@@ -13,9 +13,9 @@ let partyArray = [
     },
     {
     name: "Jesse Andersen",
-    Photo: './Images/nick.jpg',
+    Photo: './Images/Jesse.jpg',
     Role: "Groomsman/Brother of the Groom",
-    Detail: "Something Something Something"
+    Detail: "Jesse is Andrew's youngest older brother. At only 12 years older than Andrew, one can imagine how much they have in common. However, regardless of age, Andrew and Jesse have managed to be their for eachothers times in need and maintain a lasting bond."
     },
     {
     name: "Steve Pratt",
@@ -27,7 +27,7 @@ let partyArray = [
     name: "Peter Jackson", 
     Photo: './Images/IMG_20180903_134435.jpg',
     Role: "Groomsman",
-    Detail: "Something Something Something"
+    Detail: "Andrew first met Peter through the driver-side window of his car. Peter was on the off ramp of a freeway sellig oranges. Andrew, being a generous soul, offered to take Peter for a meal and a haircut. While Peter only took up the former offer, that meal was the start of a life-long friendship."
     },
     {
     name: "Alex Robertson",
@@ -39,7 +39,7 @@ let partyArray = [
     name: "Joseph Hallman",
     Photo: './Images/20180602_212724.jpg',
     Role: "Groomsman",
-    Detail: "Something Something Something"
+    Detail: "Joe-Joe-Binks is the often fogotten character who was commonly seen besides the beloved Jar-Jar Binks in Star Wars: Episode 1 - The Phantom Menace. Andrew and Joe met following Joe's fall from grace with the studio execs and his relocation to the western suburbs of Chicago."
     },
     {
     name: "Emily Wynn",
@@ -48,10 +48,10 @@ let partyArray = [
     Detail: "Met the bride during high school marching band, developing a friendship over making homemade calculus videos. Emily and Ashley have maintained a close friendship by living together at MSU and now in the Chicagoland area. Likes include MSU sports, thoughtful reading, and baking bread."
     },
     {
-    name: "Theodore Broman",
+    name: "Theodore Bromann",
     Photo: './Images/nick.jpg',
     Role: "Groomsman",
-    Detail: "Something Something Something"
+    Detail: `Theodore "Teddy" Bromann and Andrew met in kindergarden through their mutual friend, Jeff (see Best Man). Unfortunately, Teddy and Andrew have not been able to see much of eachother since Teddy's relocation to Oklahoma? Nebraska? Wait, no Kansas. For some reason, Teddy volunatarily moved to Kansas.`
     },
     {
     name: "Nick Laux",
@@ -60,15 +60,15 @@ let partyArray = [
     Detail: "Met the bride in third grade, and fostered a friendship by playing foursquare while pretending to be the Backstreet Boys. Likes include being outside, video games, and being German."
     },
     {
-    name: 'Spencer "Frank" Meyer',
+    name: 'Frank "Spencer" Meyer',
     Photo: './Images/20170412_204907.jpg',
     Role: "Groomsman",
-    Detail: "Something Something Something"
+    Detail: `Andrew and Frank met through thier mutual friend, Jeff (I'm starting to see a trend here). Intially Frank and Andrew were not too fond of eachother but eventually found a common interest in drag racing Frank's Toyota Carolla in the asphalt jungle that is Glen Ellyn. Frank has recently relocated to Las Vegas where he has rebranded himself as "Spencer", a win-at-all-costs card shark.`
     }
   ];
 
 
-  $(document).on("click", ".bridalParty", function(){
+  $(document).on("mouseover", ".bridalParty", function(){
     let value = $(this).attr("data-value")
     console.log(value)
     $("#partyName").text(partyArray[value].name)
